8eaad39dfe0eff1f618ac5e44b717ffd43326cc1
Vu Tran <vuthh79@gmail.com>
2021-10-17 21:16:48 +0700

    First commit

---
+++ src/main/java/com/vut/tradehodlbot/TradehodlbotApplication.java
@@ -0,0 +1,13 @@
+package com.vut.tradehodlbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradehodlbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradehodlbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vut/tradehodlbot/TradehodlbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vut.tradehodlbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradehodlbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
